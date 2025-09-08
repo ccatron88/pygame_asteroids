a6060c2b6734edb4598f2ea664568eac8b71a5f7
ccatron88 <christophercatron@outlook.com>
2025-09-08 15:56:06 -0500

    Add functionailty for split asteroids Added splitting of asteroids to asteroid class so that each asteroid bigger than the min radius, splits into 2 and is faster than the original asteroid

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 from circleshape import CircleShape
 import pygame
+from constants import *
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -11,3 +13,17 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        random_angle = random.uniform(20, 50)
+        random_vector1 = pygame.math.Vector2.rotate(self.velocity, random_angle)
+        random_vector2 = pygame.math.Vector2.rotate(self.velocity, -random_angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid1 = Asteroid(self.position[0], self.position[1], new_radius)
+        asteroid2 = Asteroid(self.position[0], self.position[1], new_radius)
+        asteroid1.velocity = random_vector1 * 1.2
+        asteroid2.velocity = random_vector2 * 1.2
+        
--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def main():
         for asteroid in asteroids:
             for shot in shots:
                 if asteroid.collisions(shot):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
         screen.fill("black")
         for item in drawable:
